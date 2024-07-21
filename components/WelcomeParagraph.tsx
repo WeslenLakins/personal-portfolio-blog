5cfd0532718f25a3ef214150758902e1a4aa3a72
Weslen Lakins <weslen.lakins@outlook.com>
2024-07-21 01:28:00 -0400

    Update welcome paragraph text and tag data

--- components/WelcomeParagraph.tsx
+++ components/WelcomeParagraph.tsx
@@ -4,7 +4,7 @@ export default function WelcomeParagraph() {
   return (
     <div className="py-12">
       <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-        Hi, I'm <span className="text-blue-600 dark:text-blue-400">Wes</span>!
+        Hi, I'm <span className="text-blue-600 dark:text-blue-400">Weslen</span>👋
       </h1>
       <p className="mt-6 text-lg leading-7 text-gray-500 dark:text-gray-400">
         Thank you for visiting my personal blog and portfolio. This is where I share my insights and
